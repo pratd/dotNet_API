92dcc52267c58982fdfd77becdda60af32e24663
prattya datta <pratt@LAPTOP-500LE5P5.(none)>
2021-01-15 14:01:57 +0100

    No mock repo but with Dependency Injection

---
+++ Commander/Controllers/CommandsController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Commander.Models;
+using Commander.Data;
+using System.Collections.Generic;
+
+namespace Commander.Controllers
+{
+    //api/commands
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+
+        private readonly ICommanderRepo _repository;
+
+        public CommandsController(ICommanderRepo repository)
+        {
+            _repository =  repository;
+        }
+        // private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        //GET api/commands
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+
+            return Ok(commandItems) ;
+        }
+
+        //GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
+        }
+    }
+}
--- Commander/Startup.cs
+++ Commander/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Commander.Data
 
 namespace Commander
 {
@@ -26,6 +27,8 @@ namespace Commander
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.Addscoped<ICommanderRepo, MockCommanderRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
